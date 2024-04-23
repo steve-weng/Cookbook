51cc961bf38da01dfd1d3c5b1923ac74b167ddf4
Zilin <hannahzweng@gmail.com>
2024-04-23 01:56:47 -0400

    input boxes for ingredients and steps

--- cookbook/App.js
+++ cookbook/App.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button, Text, TouchableOpacity, Keyboard } from 'react-native';
 
-const InputBox = () => {
-  const [text, setText] = useState('');
+const RecipeForm = () => {
+  // react states for ingredients and steps
+  const [ingredient, setIngredient] = useState('');
+  const [ingredientList, setIngredientList] = useState([]);
+  const [steps, setSteps] = useState('');
 
-  const handleInputChange = (inputText) => {
-    setText(inputText);
-  };
+  const handleIngredientChange = (text) => {
+    setIngredient(text);
+  }
+
+  const handleAddIngredient = () => {
+    setIngredientList([...ingredientList, ingredient.trim()]);
+    setIngredient('');
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.nativeEvent.key === 'Enter') {
+      handleAddIngredient();
+    }
+  }
+
+  const handleStepsChange = (text) => {
+    setSteps(text);
+  }
 
   //useEffect(() => {
    // fetch('/tex').then(res => res.json()).then(data => {
@@ -36,19 +54,33 @@ const InputBox = () => {
   };
 
   return (
-    <View>
+    <View style={{ padding: 20 }}>
+    <View style={{flexDirection: 'row'}}>
       <TextInput
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10, paddingHorizontal: 10 }}
-        onChangeText={handleInputChange}
-        value={text}
-        placeholder="Type something..."
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10, marginRight: 20, paddingHorizontal: 10 }}
+        onChangeText={handleIngredientChange}
+        onKeyPress={handleKeyPress}
+        value={ingredient}
+        placeholder="Enter ingredient"
       />
-      <Button
-        title="Submit"
-        onPress={handleButtonPress}
+      <TouchableOpacity onPress={handleAddIngredient}>
+        <Text style={{ color: 'blue', marginTop: 10, marginRight: 50 }}>+</Text>
+      </TouchableOpacity>
+
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10, paddingright: 50, paddingBottom: 50, paddingTop: 15, paddingLeft: 15 }}
+        onChangeText={handleStepsChange}
+        value={steps}
+        placeholder="Enter steps"
       />
+      </View>
+      <View style={{ marginTop: 10 }}>
+        {ingredientList.map((item, index) => (
+          <Text key={index}>{item}</Text>
+        ))}
+      </View>
     </View>
   );
 };
 
-export default InputBox;
+export default RecipeForm;
