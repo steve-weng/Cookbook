fda3e927613183662d1b185c3b3eee0380c9eb13
S <51041031+steve-weng@users.noreply.github.com>
2024-04-23 20:34:54 -0400

    code prep for flask login api
    code still runs just FYI untested login as we don't have frontend yet

--- cookbook/server.py
+++ cookbook/server.py
@@ -1,4 +1,10 @@
 from flask import Flask, request, render_template, jsonify
+
+# flask user login
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
+from flask_bcrypt import Bcrypt
+
 import sqlite3
 from flask_cors import CORS
 import requests
@@ -6,18 +12,85 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+# flask login stuff
+login_manager = LoginManager(app)
+bcrypt = Bcrypt(app)
+
+# db for storing users
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = 'this is a secret key '
+db = SQLAlchemy(app)
+
+# user obj
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    # is_active = db.Column(db.Boolean(), default=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+# returns user obj w/ given ID
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+# logins the user
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        jsonData = request.get_json()
+        username = jsonData['username']
+        password = jsonData['password']
+
+        # check if username exists, then if hashed password matches
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+
+            #login if so
+            login_user(user)
+            return jsonify(success=True, data="Successfully logged in")
+        
+        else:
+            return jsonify(success=False, data="Incorrect username/password")
+
+    return jsonify(success=False, data="Not even a post request")
+
+# registers the given user
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        jsonData = request.get_json()
+        username = jsonData['username']
+        password = jsonData['password']
+
+        # store username w/ hashed password in database
+        hashed_password = bcrypt.generate_password_hash(
+            password).decode('utf-8')
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify(success=True, data="Successfully Registered User")
+        #return redirect(url_for('login'))
+
+    return jsonify(success=False, data="Failed to register")
+    #return render_template('registeration.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return jsonify(success=True, data="logged out")
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def helloWorld():
     return "hello"
-    """if request.get.args('text', None):
-        print("test " + request.args['text'])
-        return "Hello world " + request.args['text']
-     params = {
-        'thing1': request.values.get('thing1'),
-        'thing2': request.get_json().get('thing2')
-    }
-    print(params)
-    return json.dumps(params) """
 
 @app.route('/index')
 def hello():
@@ -57,3 +130,9 @@ def storeRecipe():
 
     # store in database
     return jsonify(success=True, data="Recipe saved")
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
