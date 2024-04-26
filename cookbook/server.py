b521b1003e690d2589ead338395242d4d52f8de6
S <51041031+steve-weng@users.noreply.github.com>
2024-04-25 21:59:36 -0400

    saves imgurl to db

--- cookbook/server.py
+++ cookbook/server.py
@@ -138,9 +138,9 @@ def storeRecipe():
     import cloudinary.api
 
     #upload image to cloudinary, build a URL to save to DB
-    cloudinary.uploader.upload(img, public_id=recipe_name)
-    srcURL = CloudinaryImage(recipe_name).build_url()
-    print(srcURL)
+    cloudinary.uploader.upload(img, public_id=recipe_name) # recipe names should be unique
+    imgURL = CloudinaryImage(recipe_name).build_url()
+    print(imgURL)
 
     #imageFile = Image.open(img)    
     #imageFile.show()
@@ -154,20 +154,20 @@ def storeRecipe():
     #con = sqlite3.connect("test1.db")
     #cur = con.cursor()
     
-    with sqlite3.connect("test.db") as con:
+    with sqlite3.connect("test1.db") as con:
         cur = con.cursor()
-        cur.execute("CREATE TABLE if not exists Recipes(itemID integer primary key, name, ingredients, steps, img)")
+        cur.execute("CREATE TABLE if not exists Recipes(itemID integer primary key, name, ingredients, steps, imgURL)")
         cur.execute("CREATE TABLE if not exists Tags (tagID integer primary key, tagTitle)")
         cur.execute("CREATE TABLE if not exists ItemTags (itemID, tagID)")
 
-        cur.execute("INSERT INTO Recipes VALUES (NULL, ?, ?, ?, ?)", (recipe_name, ingredients, steps, img))
+        cur.execute("INSERT INTO Recipes VALUES (NULL, ?, ?, ?, ?)", (recipe_name, ingredients, steps, imgURL))
 
         recipeList = []
         for row in cur.execute("SELECT * FROM Recipes"):
         #    print(row)
             recipeList.append(row)
 
-        #print(recipeList)
+        print(recipeList)
         #for row in cur.execute("SELECT * FROM Tags"):
         #    print(row)
         #for row in cur.execute("SELECT * FROM ItemTags"):
